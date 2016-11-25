3585d8ad563b4899ca221d0d88949496b3a722cd
idk <idk@fireaxe.local>
2016-11-25 18:44:46 -0500

    changed the locations of the outputs

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 rm *.out output/*.png dandy
+rm skel/*.txt.folder/ -rf
 go build dandy.go
-for ((x = 0 ; x <= 30 ; x++)); do
-  for files in $(find skel -name *.txt); do
+for files in $(find skel -name *.txt); do
+  mkdir output
+  for ((x = 0 ; x <= 30 ; x++)); do
     echo "path=skel/colors.txt" > config.txg
     echo "path=$files" >> config.txg
     echo $x
     ./dandy &> /dev/null
   done
+  mv output $files.folder
 done
